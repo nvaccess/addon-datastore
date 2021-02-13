29a089fa02778534539e3cc995d187db9c92c4d3
nvdaes <nrm1977@gmail.com>
2021-02-13 11:09:27 +0100

    Validate sha256 against json file

--- _tools/validate.py
+++ _tools/validate.py
@@ -4,25 +4,60 @@
 # This file may be used under the terms of the GNU General Public License, version 2 or later.
 # For more details see: https://www.gnu.org/licenses/gpl-2.0.html
 
-import hashlib
 import argparse
 import typing
 import os
+import sys
 import json
-from jsonschema import validate
-#: The read size for each chunk read from the file, prevents memory overuse with large files.
-JSON_SCHEMA = os.path.join(os.path.dirname(__file__), "addonVersion_schema.json")
+import urllib.request
+from jsonschema import validate, exceptions
 
-def validateJson(jsonFile):
-	with open(JSON_SCHEMA) as f:
-		schema = json.load(f)
+sys.path.append(os.path.dirname(__file__))
+import sha256
+del sys.path[-1]
+
+JSON_SCHEMA = os.path.join(os.path.dirname(__file__), "addonVersion_schema.json")
+DOWNLOAD_BLOCK_SIZE = 8192 # 8 kb
+
+def getAddonMetadata(jsonFile):
 	with open(jsonFile) as f:
 		data = json.load(f)
+		return data
+
+def validateJson(data):
+	with open(JSON_SCHEMA) as f:
+		schema = json.load(f)
 	try:
 		validate(instance=data, schema=schema)
-		print("Add-on metadata is valid")
-	except jsonschema.exceptions.ValidationError as err:
-		print(err)
+		print("Add-on metadata matches json schema")
+	except exceptions.ValidationError as err:
+		print(f"Add-on metadata is not valid: {err}")
+
+def downloadAddon(url):
+	assert url.startswith("https"), "add-on url should start with https"
+	assert url.endswith(".nvda-addon"), "add-on url should ends with .nvda-addon"
+	destPath = os.path.join(os.path.dirname(__file__), url.split("/")[-1])
+	remote = urllib.request.urlopen(url)
+	if remote.code != 200:
+		raise RuntimeError("Download failed with code %d" % remote.code)
+	size = int(remote.headers["content-length"])
+	with open(destPath, "wb") as local:
+		read = 0
+		chunk=DOWNLOAD_BLOCK_SIZE
+		while True:
+			if size -read <chunk:
+				chunk =size -read
+			block = remote.read(chunk)
+			if not block:
+				break
+			read += len(block)
+			local.write(block)
+	return destPath
+
+def validateSha256(destPath, data):
+	with open(destPath, "rb") as f:
+		sha256Addon = sha256.sha256_checksum(f)
+		assert sha256Addon == data["sha256"], f"Please, set sha256 to {sha256Addon} in json file"
 
 
 def main():
@@ -32,7 +67,11 @@ def main():
 		help="The json (.json) file containing add-on metadata."
 	)
 	args = parser.parse_args()
-	validateJson(args.file)
+	data = getAddonMetadata(args.file)
+	validateJson(data=data)
+	url = data["URL"]
+	destPath = downloadAddon(url=url)
+	validateSha256(destPath=destPath, data=data)
 
 if __name__ == '__main__':
 	main()
