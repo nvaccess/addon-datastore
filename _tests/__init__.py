66854a9d321b27369d9a5462f2a58f660f4ccbe5
nvdaes <nrm1977@gmail.com>
2021-02-16 05:53:43 +0100

    Initial tox

---
+++ _tests/__init__.py
@@ -0,0 +1,5 @@
+import os
+import sys
+TOP_DIR = os.path.dirname(__file__)
+SOURCE_DIR = os.path.dirname(TOP_DIR)
+sys.path.append(os.path.join(SOURCE_DIR, "_tools"))
---
+++ _tests/test_validate.py
@@ -0,0 +1,19 @@
+import unittest
+import json
+from _tools import validate
+
+class TestValidate(unittest.TestCase):
+
+	def setUp(self):
+		self.jsonschema = validate.JSON_SCHEMA
+		self.template = os.path.join(SOURCE_DIR, "_tools", "21.02.json")
+		self.data = json.load(self.template)
+
+	def tearDown(self):
+		self.jsonschema = None
+		self.template = None
+		self.data = None
+
+	def test_getAddonMetadata(self):
+		data = validate.getAddonMetadata(self.template)
+		self.assertTrue(isinstance(data, dict))
