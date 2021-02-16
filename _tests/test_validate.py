27e8f2bca7ba9c08ac6f08ec2c879c15a449c685
Noelia Ruiz Martínez <nrm1977@gmail.com>
2021-02-16 09:44:49 +0100

    Fix syntax

--- _tests/test_validate.py
+++ _tests/test_validate.py
@@ -18,4 +18,4 @@ class TestValidate(unittest.TestCase):
 
 	def test_validateJson(self):
 		data = getTemplateData()
-		self.assertTrue(validate.validateJson(data)
+		self.assertTrue(validate.validateJson(data))
