0af8a713160ca56a87f475d21ca56b596461d3b4
nvdaes <nrm1977@gmail.com>
2021-02-25 20:26:22 +0100

    Add missing headers to Python files

--- validation/_tests/test_validate.py
+++ validation/_tests/test_validate.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python
 
+# Copyright (C) 2021 Noelia Ruiz Martínez, NV Access Limited
+# This file may be used under the terms of the GNU General Public License, version 2 or later.
+# For more details see: https://www.gnu.org/licenses/gpl-2.0.html
+
 import unittest
 import os
 import json
