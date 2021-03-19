a4ffc7ba7ef3d7d1598b0cdac27b72ccd56eb845
nvdaes <nrm1977@gmail.com>
2021-03-19 05:47:03 +0100

    Fix path

---
+++ validation/runValidate.bat
@@ -0,0 +1,4 @@
+@echo off
+rem validate <clipContentsDesigner\13.0.json>
+
+call "%~dp0\venvUtils\venvCmd.bat" py %1
--- validation/testValidate.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-rem validate <clipContentsDesigner\13.0.json>
-
-call "%~dp0\venvUtils\venvCmd.bat" py "%~dp0\_validate\validate.py %dp0\clipContentsDesigner\13.0.json"
