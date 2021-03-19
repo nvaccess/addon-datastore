3a277307fc267869f92503f257b8a5137a8f186a
Noelia Ruiz Martínez <nrm1977@gmail.com>
2021-03-19 14:42:35 +0100

    Indicate version of jsonschema

--- validation/_tests/__init__.py
+++
@@ -1,9 +0,0 @@
-# Copyright (C) 2021 Noelia Ruiz Martínez, NV Access Limited
-# This file may be used under the terms of the GNU General Public License, version 2 or later.
-# For more details see: https://www.gnu.org/licenses/gpl-2.0.html
-
-import os
-import sys
-TOP_DIR = os.path.dirname(__file__)
-SOURCE_DIR = os.path.dirname(TOP_DIR)
-sys.path.append(os.path.join(SOURCE_DIR, "_tools"))
--- validation/runValidate.bat
+++ validation/runValidate.bat
@@ -1,5 +1,3 @@
 @echo off
 rem validate <file.json>
-pip install wheel
 call "%~dp0\venvUtils\venvCmd.bat" py "%~dp0\_validate\validate.py" %1
-  if ERRORLEVEL 1 exit 1
