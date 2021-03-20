50bfb08b7c7419699ee0e3170044f9f2a2a23b3a
nvdaes <nrm1977@gmail.com>
2021-03-20 11:34:35 +0100

    Install wheel from batch file to avoid warnings

--- validation/runlint.bat
+++ validation/runlint.bat
@@ -1,3 +1,5 @@
 @echo off
+rem install wheel to avoid warnings
+call python -m pip install wheel
 rem lint Python files
 call "%~dp0\venvUtils\venvCmd.bat" flake8 _validate _tests --count --select=E9,F63,F7,F82 --show-source --statistics --tee
