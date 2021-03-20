aa93285a7adc054f8ca4f215ca054e4a7f25d6d5
nvdaes <nrm1977@gmail.com>
2021-03-20 11:28:57 +0100

    Add flake8 to virtual environment

--- validation/runvalidate.bat
+++ validation/runvalidate.bat
@@ -1,5 +1,5 @@
 @echo off
 rem install wheel to avoid warnings
-call pip install wheel
+call python -m pip install wheel
 rem validate <file.json>
 call "%~dp0\venvUtils\venvCmd.bat" py "%~dp0\_validate\validate.py" %1
