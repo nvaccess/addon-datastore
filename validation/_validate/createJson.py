eafcabb640e2bf6f71316486415236eed79f8282
Reef Turner <reef@nvaccess.org>
2022-07-18 18:23:28 +0800

    creating JSON with example data matches example

--- validation/_validate/createJson.py
+++ validation/_validate/createJson.py
@@ -3,21 +3,20 @@
 # Copyright (C) 2022 Noelia Ruiz Martínez, NV Access Limited
 # This file may be used under the terms of the GNU General Public License, version 2 or later.
 # For more details see: https://www.gnu.org/licenses/gpl-2.0.html
-
+import dataclasses
 import json
 import argparse
 from dataclasses import dataclass
 import os
 import sys
+
+from _validate.addonManifest import AddonManifest
+
 sys.path.append(os.path.dirname(__file__))  # To allow this module to be run as a script by runcreatejson.bat
 # E402 module level import not at top of file
 from manifestLoader import getAddonManifest  # noqa:E402
 import sha256  # noqa:E402
 del sys.path[-1]
-
-VALID_JSON = os.path.join(
-	os.path.dirname(__file__), "..", "_tests", "testData", "addons", "fake", "13.0.0.json"
-)
 
 
 @dataclass
@@ -34,74 +33,92 @@ def getSha256(addonPath: str) -> str:
 
 
 def getVersionNumber(ver: str) -> Version:
-	version = Version()
 	verParts = ver.split(".")
 	verLen = len(verParts)
 	if verLen < 2 or verLen > 3:
-		raise ValueError("Version not valid")
-	version.major = int(verParts[0])
-	version.minor = int(verParts[1])
-	if len(verParts) > 2:
-		version.patch = int(verParts[2])
+		raise ValueError(f"Version not valid: {ver}")
+	version = Version(
+		major=int(verParts[0]),
+		minor=int(verParts[1]),
+		patch=0 if len(verParts) == 2 else int(verParts[2])
+	)
 	return version
 
 
-def getFullQualifiedName(version: Version) -> str:
-	versionParts = [
-		str(version.major),
-		str(version.minor),
-		str(version.patch)
-	]
-	stringVersion = ".".join(versionParts)
-	return stringVersion
-
-
 def generateJsonFile(
-		addonPath: str, parentDir: str, channel: str,
-		publisher: str, sourceUrl: str, url: str
+		addonPath: str,
+		parentDir: str,
+		channel: str,
+		publisher: str,
+		sourceUrl: str,
+		url: str,
+		licenseName: str,
+		licenseUrl: str,
 ) -> None:
 	manifest = getAddonManifest(addonPath)
-	shaVal = getSha256(addonPath)
-	addonId = manifest["name"]
-	addonDisplayName = manifest["summary"]
-	addonDescription = manifest["description"]
-	addonHomepage = manifest["url"]
-	addonVersionNumber = manifest["version"]
-	addonMinVersion = manifest["minimumNVDAVersion"]
-	addonLastTestedVersion = manifest["lastTestedNVDAVersion"]
-	with open(VALID_JSON, "r") as f:
-		data = json.load(f)
-	del data["sha256-comment"]
-	data["addonId"] = addonId
-	data["displayName"] = addonDisplayName
-	data["description"] = addonDescription
-	data["URL"] = url
-	data["homepage"] = addonHomepage
-	data["addonVersionName"] = addonVersionNumber
-	versionNumber = getVersionNumber(addonVersionNumber)
-	filename = f"{getFullQualifiedName(versionNumber)}.json"
-	data["addonVersionNumber"]["major"] = versionNumber.major
-	data["addonVersionNumber"]["minor"] = versionNumber.minor
-	data["addonVersionNumber"]["patch"] = versionNumber.patch
-	minVersion = getVersionNumber(addonMinVersion)
-	data["minNVDAVersion"]["major"] = minVersion.major
-	data["minNVDAVersion"]["minor"] = minVersion.minor
-	data["minNVDAVersion"]["patch"] = minVersion.patch
-	lastTestedVersion = getVersionNumber(addonLastTestedVersion)
-	data["lastTestedVersion"]["major"] = lastTestedVersion.major
-	data["lastTestedVersion"]["minor"] = lastTestedVersion.minor
-	data["lastTestedVersion"]["patch"] = lastTestedVersion.patch
-	data["sha256"] = shaVal
-	data["channel"] = channel
-	data["publisher"] = publisher
-	data["sourceURL"] = sourceUrl
-	addonDir = os.path.join(parentDir, addonId)
-	if not os.path.isdir(addonDir):
-		os.makedirs(addonDir)
-	filePath = os.path.join(addonDir, filename)
+	data = _createDictMatchingJsonSchema(
+		manifest=manifest,
+		sha=getSha256(addonPath),
+		channel=channel,
+		publisher=publisher,
+		sourceUrl=sourceUrl,
+		url=url,
+		licenseName=licenseName,
+		licenseUrl=licenseUrl,
+	)
+
+	filePath = _outputFilePath(data, parentDir)
+
 	with open(filePath, "wt") as f:
 		json.dump(data, f, indent="\t")
-	print(f"Json file is in {dir}/{filename}.")
+	print(f"Wrote json file: {filePath}")
+
+
+def _outputFilePath(data, parentDir):
+	addonDir = os.path.join(parentDir, data["addonId"])
+	versionNumber = Version(**data["addonVersionNumber"])
+	canonicalVersionString = ".".join(
+		(str(i) for i in dataclasses.astuple(versionNumber))
+	)
+	if not os.path.isdir(addonDir):
+		os.makedirs(addonDir)
+	filePath = os.path.join(addonDir, f'{canonicalVersionString}.json')
+	return filePath
+
+
+def _createDictMatchingJsonSchema(
+		manifest: AddonManifest,
+		sha: str,
+		channel: str,
+		publisher: str,
+		sourceUrl: str,
+		url: str,
+		licenseName: str,
+		licenseUrl: str,
+):
+	return {  # see _validate/addonVersion_schema.json
+		"addonId": manifest["name"],
+		"displayName": manifest["summary"],
+		"URL": url,
+		"description": manifest["description"],
+		"sha256": sha,
+		"homepage": manifest["url"],
+		"addonVersionName": manifest["version"],
+		"addonVersionNumber": dataclasses.asdict(
+			getVersionNumber(manifest["version"])
+		),
+		"minNVDAVersion": dataclasses.asdict(
+			getVersionNumber(manifest["minimumNVDAVersion"])
+		),
+		"lastTestedVersion": dataclasses.asdict(
+			getVersionNumber(manifest["lastTestedNVDAVersion"])
+		),
+		"channel": channel,
+		"publisher": publisher,
+		"sourceURL": sourceUrl,
+		"license": licenseName,
+		"licenseURL": licenseUrl,
+	}
 
 
 def main():
@@ -130,14 +147,25 @@ def main():
 		dest="url",
 		help="URL to download the add-on."
 	)
+	parser.add_argument(
+		dest="licenseName",
+		help="Name of the license used with the add-on. E.G. 'GPL v2'"
+	)
+	parser.add_argument(
+		dest="licenseUrl",
+		help="URL to read the license in full. E.G. 'https://www.gnu.org/licenses/gpl-2.0.html'"
+	)
 	args = parser.parse_args()
-	filename = args.file
-	parentDir = args.parentDir
-	channel = args.channel
-	publisher = args.publisher
-	sourceUrl = args.sourceUrl
-	url = args.url
-	generateJsonFile(filename, parentDir, channel, publisher, sourceUrl, url)
+	generateJsonFile(
+		addonPath=args.file,
+		parentDir=args.parentDir,
+		channel=args.channel,
+		publisher=args.publisher,
+		sourceUrl=args.sourceUrl,
+		url=args.url,
+		licenseName=args.licenseName,
+		licenseUrl=args.licenseUrl,
+	)
 
 
 if __name__ == '__main__':
