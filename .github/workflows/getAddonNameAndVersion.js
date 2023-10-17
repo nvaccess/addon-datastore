b9c28283ed9066b30256419341045e2c3ebb363c
Noelia Ruiz Martínez <nrm1977@gmail.com>
2023-10-17 07:58:37 +0200

    Address review, partially: start moving code to js files

---
+++ .github/workflows/getAddonNameAndVersion.js
@@ -0,0 +1,12 @@
+module.exports = ({context, core}) => {
+  const fs = require('fs')
+  const addonFilename = context.needs.getAddonFilename.outputs.addonFileName
+  const contents = fs.readFileSync(addonFilename)
+  const metadata = JSON.parse(contents)
+  const addonId = metadata.addonId
+  core.setOutput('addonId', addonId)
+  const addonName = metadata.displayName
+  core.setOutput('addonName', addonName)
+  const addonVersion = metadata.addonVersionName
+  core.setOutput('addonVersion', addonVersion)
+}
