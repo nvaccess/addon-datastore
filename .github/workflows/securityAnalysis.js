bfc65da21fb6f91f200fbb84c4a898263f816882
Noelia Ruiz Martínez <nrm1977@gmail.com>
2024-05-06 21:36:56 +0200

    Unlock merge master for trusted add-ons

--- .github/workflows/securityAnalysis.js
+++ .github/workflows/securityAnalysis.js
@@ -1,5 +1,17 @@
-module.exports = ({core}, path) => {
+module.exports = ({github, core}, path) => {
   const fs = require('fs');
+  const crypto = require('crypto');
+  const addon = fs.readFileSync('addon.nvda-addon');
+  const hash = crypto.createHash('sha256');
+  hash.update(addon);
+  const hex = hash.digest('hex');
+  console.log(hex);
+  const trustedAddons = fs.readFileSync('trustedAddons.json');
+  const trustedAddonsData = JSON.parse(trustedAddons);
+  if (trustedAddonsData.trustedAddons.includes(hex)) {
+    core.info('Analysis skipped');
+   return
+  }
   const contents = fs.readFileSync(path);
   const data = JSON.parse(contents);
   const runs = data.runs[0];
@@ -7,6 +19,9 @@ module.exports = ({core}, path) => {
   if (results.length === 0) {
     core.info("Security analysis succeeded");
   } else {
+    trustedAddonsData.trustedAddons.push(hex);
+    const stringified = JSON.stringify(trustedAddonsData, null, 2);
+    fs.writeFileSync('trustedAddons.json', stringified);
     core.setFailed("Security analysis failed");
   }
 };
