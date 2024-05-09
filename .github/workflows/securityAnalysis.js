97b47deab7a44aa33d95fc374de03bf121c42e6f
Noelia Ruiz Martínez <nrm1977@gmail.com>
2024-05-09 22:00:48 +0200

    Improvements to manual approval process

--- .github/workflows/securityAnalysis.js
+++ .github/workflows/securityAnalysis.js
@@ -1,4 +1,4 @@
-module.exports = ({github, core}, path) => {
+module.exports = ({core}, path) => {
   const fs = require('fs');
   const addonMetadataContents = fs.readFileSync('addonMetadata.json');
   const addonMetadata = JSON.parse(addonMetadataContents);
@@ -6,9 +6,9 @@ module.exports = ({github, core}, path) => {
   const sha256 = addonMetadata.sha256;
   const reviewedAddonsContents = fs.readFileSync('reviewedAddons.json');
   const reviewedAddonsData = JSON.parse(reviewedAddonsContents);
-  if (reviewedAddonsData.addonId !== undefined && reviewedAddonsData.addonId.includes(sha256)) {
+  if (reviewedAddonsData[addonId] !== undefined && reviewedAddonsData[addonId].includes(sha256)) {
     core.info('Analysis skipped');
-   return
+   return;
   }
   const contents = fs.readFileSync(path);
   const data = JSON.parse(contents);
@@ -16,13 +16,13 @@ module.exports = ({github, core}, path) => {
   const results = runs.results;
   if (results.length === 0) {
     core.info("Security analysis succeeded");
-  } else {
-    if (reviewedAddonsData.addonId === undefined) {
-      reviewedAddonsData.id = [];
-	}
-    reviewedAddonsData.addonId.push(sha256);
-    const stringified = JSON.stringify(reviewedAddonsData, null, 2);
-    fs.writeFileSync('reviewedAddons.json', stringified);
-    core.setFailed("Security analysis failed");
+    return;
   }
+  if (reviewedAddonsData[addonId] === undefined) {
+    reviewedAddonsData[addonId] = [];
+	}
+  reviewedAddonsData[addonId].push(sha256);
+  const stringified = JSON.stringify(reviewedAddonsData, null, 2);
+  fs.writeFileSync('reviewedAddons.json', stringified);
+  core.setFailed("Security analysis failed");
 };
