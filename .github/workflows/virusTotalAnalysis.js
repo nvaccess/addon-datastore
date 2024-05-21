8a5e13834577c70837800be2cca89a185bbf8455
Noelia Ruiz Martínez <nrm1977@gmail.com>
2024-05-21 22:00:22 +0200

    Use sha256 of add-on metadata

--- .github/workflows/virusTotalAnalysis.js
+++ .github/workflows/virusTotalAnalysis.js
@@ -5,12 +5,13 @@ module.exports = ({core}) => {
   const addonMetadata = JSON.parse(addonMetadataContents);
   const addonId = addonMetadata.addonId;
   core.setOutput('addonId', addonId);
-  const sha256 = addonMetadata.sha256;
+  // const sha256 = addonMetadata.sha256;
+  const sha256 = '42335e36a209d39905414f0cbc71aa692338e3bf63efce8bc68d6949d2994ccd';
   const analysisUrl = `https://www.virustotal.com/gui/file/${sha256}`;
   core.setOutput('analysisUrl', analysisUrl);
-  const falsePositiveAddonsContents = fs.readFileSync('falsePositiveAddons.json');
-  const falsePositiveAddonsData = JSON.parse(falsePositiveAddonsContents);
-  if (falsePositiveAddonsData[addonId] !== undefined && falsePositiveAddonsData[addonId].includes(sha256)) {
+  const reviewedAddonsContents = fs.readFileSync('reviewedAddons.json');
+  const reviewedAddonsData = JSON.parse(reviewedAddonsContents);
+  if (reviewedAddonsData[addonId] !== undefined && reviewedAddonsData[addonId].includes(sha256)) {
     core.info('VirusTotal analysis skipped');
     return;
   }
@@ -24,12 +25,12 @@ module.exports = ({core}) => {
       core.info('VirusTotal analysis succeeded');
       return;
     }
-    if (falsePositiveAddonsData[addonId] === undefined) {
-      falsePositiveAddonsData[addonId] = [];
+    if (reviewedAddonsData[addonId] === undefined) {
+      reviewedAddonsData[addonId] = [];
     }
-    falsePositiveAddonsData[addonId].push(sha256);
-    stringified = JSON.stringify(falsePositiveAddonsData, null, 2);
-    fs.writeFileSync('falsePositiveAddons.json', stringified);
+    reviewedAddonsData[addonId].push(sha256);
+    stringified = JSON.stringify(reviewedAddonsData, null, 2);
+    fs.writeFileSync('reviewedAddons.json', stringified);
     core.setFailed('VirusTotal analysis failed');
   });
 };
