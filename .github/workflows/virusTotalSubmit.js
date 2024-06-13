a7395459d24060160f333682329d10f094b05dcc
Sean Budd <sean@nvaccess.org>
2024-06-13 10:34:42 +1000

    Scan all add-ons with VT

---
+++ .github/workflows/virusTotalSubmit.js
@@ -0,0 +1,38 @@
+const glob = require('glob');
+
+module.exports = ({core}, globPattern) => {
+  const fs = require('fs');
+  const { exec } = require('child_process');
+  files = glob.globSync(globPattern);
+  files.forEach(file => {
+    const addonMetadataContents = fs.readFileSync(file);
+    const addonMetadata = JSON.parse(addonMetadataContents);
+    const addonId = addonMetadata.addonId;
+    const sha256 = addonMetadata.sha256;
+    exec(`vt file ${sha256} -k ${process.env.VT_API_KEY} --format json`, (err, stdout, stderr) => {
+      if (stderr === '' || err === null) {
+        // File has been scanned before
+        return;
+      }
+      console.log(`err: ${err}`);
+      console.log(`stdout: ${stdout}`);
+      console.log(`stderr: ${stderr}`);
+      // download add-on file
+      exec(`curl --location --output ${addonId}.nvda-addon "${addonMetadata.URL}"`, (err, stdout, stderr) => {
+        if (stderr !== '' || err !== null) {
+          console.log(`err: ${err}`);
+          console.log(`stdout: ${stdout}`);
+          console.log(`stderr: ${stderr}`);
+          core.setFailed('Failed to download add-on file');
+          return;
+        }
+        // scan downloaded file
+        exec(`vt scan file -k ${process.env.VT_API_KEY} ${addonId}.nvda-addon`, (err, stdout, stderr) => {
+          console.log(`err: ${err}`);
+          console.log(`stdout: ${stdout}`);
+          console.log(`stderr: ${stderr}`);
+        })
+      })
+    });
+  });
+};
