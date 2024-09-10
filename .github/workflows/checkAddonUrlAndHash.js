1e8e8ad7b8bd37b3831b137f024f6fe19af879c1
Sean Budd <sean@nvaccess.org>
2024-09-10 19:00:18 +1000

    code cleanup, added comments

--- .github/workflows/checkAddonUrlAndHash.js
+++ .github/workflows/checkAddonUrlAndHash.js
@@ -7,11 +7,12 @@ const PROJECT_URL = "https://github.com/nvaccess/addon-datastore/blob/master/";
 
 function removeMetadataFile({core}, metadataFile, reason) {
   console.log(`Deleting file "${metadataFile}" because ${reason}`);
+  // normalise the Windows-style path to be usable for the URL in the PR body
+  const metadataFileNormalised = metadataFile.replace(/\\/g, '/');
+  core._PRBodyString += `| [${metadataFileNormalised.replace("addons/", "")}](${PROJECT_URL}${metadataFileNormalised}) | ${reason} |\n`;
+  core.setOutput("PRBodyString", core._PRBodyString);
   exec(`rm "${metadataFile}"`, (err, stdout, stderr) => {
     // stdout is garbage here, so we don't use it
-    metadataFileNormalised = metadataFile.replace(/\\/g, '/');
-    core._PRBodyString += `| [${metadataFileNormalised.replace("addons/", "")}](${PROJECT_URL}${metadataFileNormalised}) | ${reason} |\n`;
-    core.setOutput("PRBodyString", core._PRBodyString);
     if (stderr !== '' || err !== null) {
       console.log(`err: ${err}`);
       console.log(`stderr: ${stderr}`);
@@ -23,9 +24,9 @@ function removeMetadataFile({core}, metadataFile, reason) {
 
 function removeDownloadedAddonFile(downloadFileName, metadataFile) {
   exec(`rm "${downloadFileName}"`, (err, stdout, stderr) => {
+    // stdout is garbage here, so we don't use it
     if (stderr !== '' || err !== null) {
       console.log(`err: ${err}`);
-      console.log(`stdout: ${stdout}`);
       console.log(`stderr: ${stderr}`);
       console.error(`Failed to delete downloaded add-on file for ${metadataFile}`);
       return;
@@ -36,7 +37,6 @@ function removeDownloadedAddonFile(downloadFileName, metadataFile) {
 function checkDownloadedAddonHash({core}, downloadFileName, metadataFile, sha256) {
   // if hash mismatches, delete the addon data file
   const hash = crypto.createHash('sha256');
-  hash.write("")
   hash.setEncoding('hex');
   const fileStream = fs.createReadStream(downloadFileName);
   fileStream.on('data', chunk => {
@@ -45,7 +45,7 @@ function checkDownloadedAddonHash({core}, downloadFileName, metadataFile, sha256
   fileStream.on('end', () => {
     const fileHash = hash.digest('hex');
     hash.end();
-    // delete downloaded file
+    // Clean up the downloaded file
     removeDownloadedAddonFile(downloadFileName, metadataFile);
     if (fileHash.toLowerCase() !== sha256.toLowerCase()) {
       removeMetadataFile({core}, metadataFile, `Hash mismatch ${fileHash} (actual) != ${sha256} (expected)`);
@@ -55,12 +55,13 @@ function checkDownloadedAddonHash({core}, downloadFileName, metadataFile, sha256
 }
 
 function checkMetadataDownloadResult({core}, metadataFile, downloadFileName, sha256, err, stdout, stderr) {
+  // if download fails, delete the addon metadata file
   if (stderr !== '' || err !== null) {
     console.log(`err: ${err}`);
     console.log(`stdout: ${stdout}`);
     console.log(`stderr: ${stderr}`);
-    // delete file if download failed
-    strippedStderr = stderr.replace("\n", "  ").replace("\r", "");
+    // strip newlines and carriage returns from stderr to avoid breaking the markdown table
+    const strippedStderr = stderr.replace("\n", "  ").replace("\r", "");
     removeMetadataFile({core}, metadataFile, `Download failed: ${strippedStderr}`); 
     return;
   }
@@ -68,15 +69,17 @@ function checkMetadataDownloadResult({core}, metadataFile, downloadFileName, sha
   checkDownloadedAddonHash({core}, downloadFileName, metadataFile, sha256);
 }
 
-function checkMetadataFile({core}, metadataFile) {
+function checkAddonDownload({core}, metadataFile) {
   const addonMetadataContents = fs.readFileSync(metadataFile);
   const addonMetadata = JSON.parse(addonMetadataContents);
   const sha256 = addonMetadata.sha256;
+  // We need a unique name otherwise we could overwrite files
   const downloadFileName = `${uuidv4()}.nvda-addon`;
   // if download fails, delete the addon data file
   exec(
     `curl --fail --silent --show-error --location --output "${downloadFileName}" "${addonMetadata.URL}"`,
-    // increase maxBuffer size to 10GB
+    // increase maxBuffer size to 10GB as add-on files can be large,
+    // and we need to read the entire file to calculate the hash
     { maxBuffer: 1024 * 1024 * 1024 * 10 },
     (err, stdout, stderr) => {
       checkMetadataDownloadResult({core}, metadataFile, downloadFileName, sha256, err, stdout, stderr);
@@ -84,9 +87,10 @@ function checkMetadataFile({core}, metadataFile) {
 }
 
 module.exports = ({core}, globPattern) => {
+  // We want to build a table of files that are being removed
   core._PRBodyString = "| File | Reason |\n|---|---|\n";
   const metadataFiles = glob.globSync(globPattern);
   metadataFiles.forEach(metadataFile => {
-    checkMetadataFile({core}, metadataFile);
+    checkAddonDownload({core}, metadataFile);
   });
 };
