fcd75a40ef5b5ced87fdbcf4d05046a300e3993c
Sean Budd <sean@nvaccess.org>
2024-09-25 10:51:48 +1000

    review fixups

--- .github/workflows/virusTotalAnalysis.js
+++ .github/workflows/virusTotalAnalysis.js
@@ -1,45 +1,44 @@
-const glob = require('glob');
-const fs = require('fs');
-const { exec } = require('child_process');
+const glob = require("glob");
+const fs = require("fs");
+const { exec } = require("child_process");
+const countAPIUsageAndWait = require("./virusTotalAPISleepAndCount");
 
-function sleep(n) {
-  Atomics.wait(new Int32Array(new SharedArrayBuffer(4)), 0, 0, n);
-}
-
-function countAPIUsageAndWait({core}) {
-  // Sleep 20 seconds to avoid rate limiting
-  sleep(20 * 1000);
-  core._apiUsageCount++;
-}
 
 function writeVTScanUrl({core}, metadataFile, addonMetadata) {
-  // Write vtScanUrl to file
   const vtScanUrl = `https://www.virustotal.com/gui/file/${addonMetadata.sha256}`;
   addonMetadata.vtScanUrl = vtScanUrl;
   stringified = JSON.stringify(addonMetadata, null, "\t");
+  // Write vtScanUrl to add-on metadata file
   fs.writeFileSync(metadataFile, stringified);
   // Store the latest vtScanUrl for single file analysis
-  core.setOutput('vtScanUrl', vtScanUrl);
+  core.setOutput("vtScanUrl", vtScanUrl);
 }
 
+
 function getVirusTotalAnalysis({core}, addonMetadata, metadataFile, reviewedAddonsData) {
+  /*
+  Get the VirusTotal analysis for the add-on file.
+  If the add-on is flagged as malicious, store the sha256 hash in reviewedAddons.json.
+  Always store the scan URL in the add-on metadata file.
+  If Virus total fails to scan the add-on, fail the job.
+  */
   countAPIUsageAndWait({core});
   exec(`vt file ${addonMetadata.sha256} -k ${process.env.VT_API_KEY} --format json`, (err, stdout, stderr) => {
-    if (stderr !== '' || err !== null) {
+    if (stderr !== "" || err !== null) {
       console.log(`err: ${err}`);
       console.log(`stdout: ${stdout}`);
       console.log(`stderr: ${stderr}`);
-      core.setFailed('Failed to get VirusTotal analysis');
+      core.setFailed("Failed to get VirusTotal analysis");
       return;
     }
     writeVTScanUrl({core}, metadataFile, addonMetadata);
     // Append the VirusTotal analysis to the file for an artifact
     const vtData = JSON.parse(stdout);
-    fs.appendFileSync('vt.json', stdout);
+    fs.appendFileSync("vt.json", stdout);
     const stats = vtData[0]["last_analysis_stats"];
     const malicious = stats.malicious;
     if (malicious === 0) {
-      core.info('VirusTotal analysis succeeded');
+      core.info("VirusTotal analysis succeeded");
       return;
     }
     if (reviewedAddonsData[addonMetadata.addonId] === undefined) {
@@ -47,24 +46,32 @@ function getVirusTotalAnalysis({core}, addonMetadata, metadataFile, reviewedAddo
     }
     reviewedAddonsData[addonMetadata.addonId].push(addonMetadata.sha256);
     stringified = JSON.stringify(reviewedAddonsData, null, "\t");
-    fs.writeFileSync('reviewedAddons.json', stringified);
-    core.setFailed('VirusTotal analysis failed');
+    fs.writeFileSync("reviewedAddons.json", stringified);
+    core.setFailed("VirusTotal analysis failed");
   });
 }
 
+
 function getVirusTotalAnalysisIfRequired({core}, metadataFile) {
+  /*
+  If we have scanned and stored the VirusTotal analysis for the add-on before,
+  skip the analysis. Otherwise, get the VirusTotal analysis and store the URL
+  in the add-on metadata.
+  */
   const addonMetadataContents = fs.readFileSync(metadataFile);
   const addonMetadata = JSON.parse(addonMetadataContents);
   const addonId = addonMetadata.addonId;
-  const reviewedAddonsContents = fs.readFileSync('reviewedAddons.json');
+  const reviewedAddonsContents = fs.readFileSync("reviewedAddons.json");
   const reviewedAddonsData = JSON.parse(reviewedAddonsContents);
+  // Check if add-on has been flagged before through VirusTotal.
   if (reviewedAddonsData[addonId] !== undefined && reviewedAddonsData[addonId].includes(sha256)) {
-    core.info('VirusTotal analysis skipped, already performed');
+    core.info("VirusTotal analysis skipped, already performed");
     return;
   }
-  if (core._apiUsageCount >= 10) {
-    core.info('VirusTotal API usage limit reached');
-    throw new Error('VirusTotal API usage limit reached');
+  // Check if add-on has been scanned before through VirusTotal.
+  if (addonMetadata.vtScanUrl !== undefined) {
+    core.info("VirusTotal analysis skipped, already performed");
+    return;
   }
   getVirusTotalAnalysis({core}, addonMetadata, metadataFile, reviewedAddonsData);
 }
--- .github/workflows/virusTotalSubmit.js
+++ .github/workflows/virusTotalSubmit.js
@@ -1,22 +1,17 @@
-const glob = require('glob');
-const { v4: uuidv4 } = require('uuid');
-const fs = require('fs');
-const { exec } = require('child_process');
-
-
-function sleep(n) {
-  Atomics.wait(new Int32Array(new SharedArrayBuffer(4)), 0, 0, n);
-}
-
-
-function countAPIUsageAndWait({core}) {
-  // Sleep 20 seconds to avoid rate limiting
-  sleep(20 * 1000);
-  core._apiUsageCount++;
-}
+const glob = require("glob");
+const { v4: uuidv4 } = require("uuid");
+const fs = require("fs");
+const { exec } = require("child_process");
+const countAPIUsageAndWait = require("./virusTotalAPISleepAndCount");
 
 
 function submitAddon({core}, addonMetadata, downloadFileName) {
+  /*
+  Submit add-on file to VirusTotal for scanning.
+  Removes downloaded add-on file after scanning.
+  Later we will confirm the virus scan results with another API call.
+  If add-on submission fails we will continue.
+  */
   countAPIUsageAndWait({core});
   // scan downloaded file
   exec(
@@ -25,7 +20,7 @@ function submitAddon({core}, addonMetadata, downloadFileName) {
     { maxBuffer: 1024 * 1024 * 1024 * 10 },
     (err, stdout, stderr) => {
     removeDownloadedAddonFile(downloadFileName, addonMetadata.URL);
-    if (stderr !== '' || err !== null) {
+    if (stderr !== "" || err !== null) {
       console.log(`err: ${err}`);
       console.log(`stdout: ${stdout}`);
       console.log(`stderr: ${stderr}`);
@@ -39,7 +34,7 @@ function submitAddon({core}, addonMetadata, downloadFileName) {
 function removeDownloadedAddonFile(downloadFileName, metadataFile) {
   exec(`rm "${downloadFileName}"`, (err, stdout, stderr) => {
     // stdout is garbage here, so we don't use it
-    if (stderr !== '' || err !== null) {
+    if (stderr !== "" || err !== null) {
       console.log(`err: ${err}`);
       console.log(`stderr: ${stderr}`);
       console.error(`Failed to delete downloaded add-on file for ${metadataFile}`);
@@ -48,7 +43,14 @@ function removeDownloadedAddonFile(downloadFileName, metadataFile) {
   })
 }
 
+
 function downloadAndSubmitAddon({core}, addonMetadata) {
+  /*
+  Download add-on file to send to VirusTotal for scanning.
+  Removes downloaded add-on file after scanning.
+  Later we will confirm the virus scan results with another API call.
+  If add-on download or submission fails we will continue.
+  */
   // We need a unique name otherwise we could overwrite files
   const downloadFileName = `${uuidv4()}.nvda-addon`;
   exec(
@@ -56,7 +58,7 @@ function downloadAndSubmitAddon({core}, addonMetadata) {
     // increase maxBuffer size to 10GB as add-on files can be large
     { maxBuffer: 1024 * 1024 * 1024 * 10 },
     (err, stdout, stderr) => {
-    if (stderr !== '' || err !== null) {
+    if (stderr !== "" || err !== null) {
       console.log(`err: ${err}`);
       console.log(`stdout: ${stdout}`);
       console.log(`stderr: ${stderr}`);
@@ -69,13 +71,13 @@ function downloadAndSubmitAddon({core}, addonMetadata) {
 
 
 function submitAddonIfNotScanned({core}, metadataFile) {
+  /*
+  Check if add-on has been scanned before through VirusTotal.
+  If not, download and submit the add-on.
+  */
   const addonMetadataContents = fs.readFileSync(metadataFile);
   const addonMetadata = JSON.parse(addonMetadataContents);
   const sha256 = addonMetadata.sha256;
-  if (core._apiUsageCount >= 10) {
-    core.info('VirusTotal API usage limit reached');
-    throw new Error('VirusTotal API usage limit reached');
-  }
   countAPIUsageAndWait({core});
   // Check if file has been scanned before
   exec(`vt file ${sha256} -k ${process.env.VT_API_KEY} --format json`, (err, stdout, stderr) => {
@@ -94,6 +96,7 @@ function submitAddonIfNotScanned({core}, metadataFile) {
     }
   });
 }
+
 
 module.exports = ({core}, globPattern) => {
   const metadataFiles = glob.globSync(globPattern);
