7d72b19fae5e9a4284022994209e9eb629068b64
Sean Budd <sean@nvaccess.org>
2026-03-24 17:51:43 +1100

    remove generateData

--- transform/src/tests/generateData.py
+++
@@ -1,4 +0,0 @@
-# Copyright (C) 2021 NV Access Limited
-# This file may be used under the terms of the GNU General Public License, version 2 or later.
-# For more details see: https://www.gnu.org/licenses/gpl-2.0.html
-
