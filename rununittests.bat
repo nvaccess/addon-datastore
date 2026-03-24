bbac6c499596b9d039f88f331900117918ad09f0
Sean Budd <sean@nvaccess.org>
2026-03-24 16:52:00 +1100

    fix test path

--- rununittests.bat
+++ rununittests.bat
@@ -3,7 +3,7 @@ set hereOrig=%~dp0
 set here=%hereOrig%
 if #%hereOrig:~-1%# == #\# set here=%hereOrig:~0,-1%
 set validationUnitTestsPath=%here%\validation\tests
-set transformUnitTestsPath=%here%\transform\tests
+set transformUnitTestsPath=%here%\transform\src\tests
 set testOutput=%here%\testOutput
 md %testOutput%
 
